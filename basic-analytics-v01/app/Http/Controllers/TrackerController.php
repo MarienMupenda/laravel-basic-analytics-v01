521ab34a2d32c7527f81bd68cb7e726d4efba5d9
Marien Mupenda <marien.mupenda@gmail.com>
2022-04-19 01:28:12 +0200

    delegate job

--- basic-analytics-v01/app/Http/Controllers/TrackerController.php
+++ basic-analytics-v01/app/Http/Controllers/TrackerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreTrackerRequest;
 use App\Http\Requests\UpdateTrackerRequest;
+use App\Jobs\TrackHitJob;
 use App\Models\Hit;
 use App\Models\Tracker;
 use Illuminate\Http\Request;
@@ -14,17 +15,6 @@ class TrackerController extends Controller
     //track
     public function track(Request $request, Tracker $tracker)
     {
-        $url = $request->url();
-        $hit = Hit::create([
-            'url' => $url,
-            'tracker_id' => $tracker->id,
-        ]);
-        $previousHit = Hit::where('tracker_id', $tracker->id)->orderBy('id', 'desc')->skip(1)->first();
-        if ($previousHit) {
-            $previousHit->seconds = $hit->created_at->diffInSeconds($previousHit->created_at);
-            $previousHit->save();
-            return response()->json(['seconds' => $previousHit->seconds]);
-        }
-        return 0;
+        TrackHitJob::dispatch($request, $tracker);
     }
 }
--- basic-analytics-v01/app/Jobs/TrackHitJob.php
+++ basic-analytics-v01/app/Jobs/TrackHitJob.php
@@ -2,10 +2,13 @@
 
 namespace App\Jobs;
 
+use App\Models\Hit;
+use App\Models\Tracker;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Http\Request;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
@@ -13,14 +16,18 @@ class TrackHitJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    private $url;
+    private $tracker_id;
+
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Request $request, Tracker $tracker)
     {
-        //
+        $this->url = $request->url();
+        $this->tracker_id = $tracker->id;
     }
 
     /**
@@ -30,6 +37,17 @@ class TrackHitJob implements ShouldQueue
      */
     public function handle()
     {
-        //
+
+        $hit = Hit::create([
+            'url' => $this->url,
+            'tracker_id' => $this->tracker_id,
+        ]);
+        $previousHit = Hit::where('tracker_id', $this->tracker_id)->orderBy('id', 'desc')->skip(1)->first();
+        if ($previousHit) {
+            $previousHit->seconds = $hit->created_at->diffInSeconds($previousHit->created_at);
+            $previousHit->save();
+            return response()->json(['seconds' => $previousHit->seconds]);
+        }
+        return 0;
     }
 }
